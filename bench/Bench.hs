93dd580c8e2386447227ec9fc1a15446e799946a
Bodigrim <andrew.lelechenko@gmail.com>
2020-01-11 19:09:56 +0000

    Expose benchmarks

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE DataKinds #-}
 
+{-# OPTIONS_GHC -fno-warn-type-defaults -fno-warn-name-shadowing #-}
+
 module Main where
 
-import Data.List
 import Data.Maybe
 import Data.Time.Clock
+import System.IO
 
 import qualified Data.Mod
 import qualified Data.Mod.Word
-import qualified Data.Modular
-import qualified Numeric.Modular
+-- import qualified Data.Modular
+-- import qualified Numeric.Modular
 
 benchAddition :: IO ()
 benchAddition = do
@@ -18,26 +20,22 @@ benchAddition = do
   t0 <- getCurrentTime
   print (sum [1..10^7] :: Data.Mod.Word.Mod 1000000007)
   t1 <- getCurrentTime
-  putStrLn "Data.Mod.Word"
-  print $ diffUTCTime t1 t0
+  putStrLn $ "Data.Mod.Word   " ++ show (diffUTCTime t1 t0)
 
   t0 <- getCurrentTime
   print (sum [1..10^7] :: Data.Mod.Mod 1000000007)
   t1 <- getCurrentTime
-  putStrLn "Data.Mod"
-  print $ diffUTCTime t1 t0
+  putStrLn $ "Data.Mod        " ++ show (diffUTCTime t1 t0)
 
-  t0 <- getCurrentTime
-  print (sum [1..10^7] :: Data.Modular.Mod Integer 1000000007)
-  t1 <- getCurrentTime
-  putStrLn "Data.Modular"
-  print $ diffUTCTime t1 t0
+  -- t0 <- getCurrentTime
+  -- print (sum [1..10^7] :: Data.Modular.Mod Integer 1000000007)
+  -- t1 <- getCurrentTime
+  -- putStrLn $ "Data.Modular    " ++ show (diffUTCTime t1 t0)
 
-  t0 <- getCurrentTime
-  print (sum (map fromIntegral [1..10^7]) :: Numeric.Modular.Mod 1000000007)
-  t1 <- getCurrentTime
-  putStrLn "Numeric.Modular"
-  print $ diffUTCTime t1 t0
+  -- t0 <- getCurrentTime
+  -- print (sum (map fromIntegral [1..10^7]) :: Numeric.Modular.Mod 1000000007)
+  -- t1 <- getCurrentTime
+  -- putStrLn $ "Numeric.Modular " ++ show (diffUTCTime t1 t0)
 
 benchProduct :: IO ()
 benchProduct = do
@@ -46,26 +44,22 @@ benchProduct = do
   t0 <- getCurrentTime
   print (product [1..10^7] :: Data.Mod.Word.Mod 1000000007)
   t1 <- getCurrentTime
-  putStrLn "Data.Mod.Word"
-  print $ diffUTCTime t1 t0
+  putStrLn $ "Data.Mod.Word   " ++ show (diffUTCTime t1 t0)
 
   t0 <- getCurrentTime
   print (product [1..10^7] :: Data.Mod.Mod 1000000007)
   t1 <- getCurrentTime
-  putStrLn "Data.Mod"
-  print $ diffUTCTime t1 t0
+  putStrLn $ "Data.Mod        " ++ show (diffUTCTime t1 t0)
 
-  t0 <- getCurrentTime
-  print (product [1..10^7] :: Data.Modular.Mod Integer 1000000007)
-  t1 <- getCurrentTime
-  putStrLn "Data.Modular"
-  print $ diffUTCTime t1 t0
+  -- t0 <- getCurrentTime
+  -- print (product [1..10^7] :: Data.Modular.Mod Integer 1000000007)
+  -- t1 <- getCurrentTime
+  -- putStrLn $ "Data.Modular    " ++ show (diffUTCTime t1 t0)
 
-  t0 <- getCurrentTime
-  print (product (map fromIntegral [1..10^7]) :: Numeric.Modular.Mod 1000000007)
-  t1 <- getCurrentTime
-  putStrLn "Numeric.Modular"
-  print $ diffUTCTime t1 t0
+  -- t0 <- getCurrentTime
+  -- print (product (map fromIntegral [1..10^7]) :: Numeric.Modular.Mod 1000000007)
+  -- t1 <- getCurrentTime
+  -- putStrLn $ "Numeric.Modular " ++ show (diffUTCTime t1 t0)
 
 benchInversion :: IO ()
 benchInversion = do
@@ -74,20 +68,17 @@ benchInversion = do
   t0 <- getCurrentTime
   print (sum (map (fromJust . Data.Mod.Word.invertMod) [1 ..10^6]) :: Data.Mod.Word.Mod 1000000007)
   t1 <- getCurrentTime
-  putStrLn "Data.Mod.Word"
-  print $ diffUTCTime t1 t0
+  putStrLn $ "Data.Mod.Word   " ++ show (diffUTCTime t1 t0)
 
   t0 <- getCurrentTime
   print (sum (map (fromJust . Data.Mod.invertMod) [1 ..10^6]) :: Data.Mod.Mod 1000000007)
   t1 <- getCurrentTime
-  putStrLn "Data.Mod"
-  print $ diffUTCTime t1 t0
+  putStrLn $ "Data.Mod        " ++ show (diffUTCTime t1 t0)
 
-  t0 <- getCurrentTime
-  print (sum (map Data.Modular.inv [1..10^6]) :: Data.Modular.Mod Integer 1000000007)
-  t1 <- getCurrentTime
-  putStrLn "Data.Modular"
-  print $ diffUTCTime t1 t0
+  -- t0 <- getCurrentTime
+  -- print (sum (map Data.Modular.inv [1..10^6]) :: Data.Modular.Mod Integer 1000000007)
+  -- t1 <- getCurrentTime
+  -- putStrLn $ "Data.Modular    " ++ show (diffUTCTime t1 t0)
 
 benchPower :: IO ()
 benchPower = do
@@ -96,30 +87,30 @@ benchPower = do
   t0 <- getCurrentTime
   print (sum (map (2 ^) [1..10^6]) :: Data.Mod.Word.Mod 1000000007)
   t1 <- getCurrentTime
-  putStrLn "Data.Mod.Word"
-  print $ diffUTCTime t1 t0
+  putStrLn $ "Data.Mod.Word   " ++ show (diffUTCTime t1 t0)
 
   t0 <- getCurrentTime
   print (sum (map (2 ^) [1..10^6]) :: Data.Mod.Mod 1000000007)
   t1 <- getCurrentTime
-  putStrLn "Data.Mod"
-  print $ diffUTCTime t1 t0
+  putStrLn $ "Data.Mod        " ++ show (diffUTCTime t1 t0)
 
-  t0 <- getCurrentTime
-  print (sum (map (2 ^) [1..10^6]) :: Data.Modular.Mod Integer 1000000007)
-  t1 <- getCurrentTime
-  putStrLn "Data.Modular"
-  print $ diffUTCTime t1 t0
+  -- t0 <- getCurrentTime
+  -- print (sum (map (2 ^) [1..10^6]) :: Data.Modular.Mod Integer 1000000007)
+  -- t1 <- getCurrentTime
+  -- putStrLn $ "Data.Modular    " ++ show (diffUTCTime t1 t0)
 
-  t0 <- getCurrentTime
-  print (sum (map (2 ^) [1..10^6]) :: Numeric.Modular.Mod 1000000007)
-  t1 <- getCurrentTime
-  putStrLn "Numeric.Modular"
-  print $ diffUTCTime t1 t0
+  -- t0 <- getCurrentTime
+  -- print (sum (map (2 ^) [1..10^6]) :: Numeric.Modular.Mod 1000000007)
+  -- t1 <- getCurrentTime
+  -- putStrLn $ "Numeric.Modular " ++ show (diffUTCTime t1 t0)
 
 main :: IO ()
 main = do
+  hSetBuffering stdout LineBuffering
   benchAddition
+  putStrLn ""
   benchProduct
+  putStrLn ""
   benchInversion
+  putStrLn ""
   benchPower
