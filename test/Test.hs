451a6d3d830074e0f44bd3bd57b0eec0a197b87e
Bodigrim <andrew.lelechenko@gmail.com>
2023-02-07 00:48:45 +0000

    Extend test suite

--- test/Test.hs
+++ test/Test.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE TypeApplications    #-}
 {-# LANGUAGE TypeOperators       #-}
 
-{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# OPTIONS_GHC -Wno-orphans -Wno-type-defaults #-}
 
 module Main (main) where
 
@@ -15,7 +15,7 @@ import qualified Data.Mod.Word as Word
 import Data.Proxy
 import Data.Semigroup
 import Foreign.Storable (Storable(..))
-import GHC.TypeNats (KnownNat, SomeNat(..), natVal, someNatVal, type (^))
+import GHC.TypeNats (KnownNat, SomeNat(..), natVal, someNatVal, type (^), type (+), type (-))
 import Test.Tasty
 import Test.Tasty.QuickCheck
 import Test.QuickCheck.Classes.Base
@@ -33,42 +33,44 @@ import Data.Vector.Unboxed (Unbox)
 import Test.QuickCheck.Classes (muvectorLaws, primLaws)
 #endif
 
+#define testModLabeled(lbl, n) \
+  testGroup ("Mod " ++ lbl) $ \
+    testProperty "fromInteger" \
+      (fromIntegerProp (Proxy :: Proxy (n))) : \
+    testProperty "invertMod"   (invertModProp   @(n)) : \
+    testProperty "powMod"      (powModProp      @(n)) : \
+    map lawsToTest (laws (Proxy :: Proxy (Mod (n))))
+
+#define testMod(n) testModLabeled(show (n :: Integer), n)
+
+#define testModWordLabeled(lbl, n) \
+  testGroup ("Word.Mod" ++ lbl) $ \
+    testProperty "fromInteger" \
+      (fromIntegerWordProp (Proxy :: Proxy (n))) : \
+    testProperty "powMod"    (powModWordProp    @(n)) : \
+    testProperty "invertMod" (invertModWordProp @(n)) : \
+    map lawsToTest (laws (Proxy :: Proxy (Word.Mod (n))))
+
+#define testModWord(n) testModWordLabeled(show (n :: Integer), n)
+
 main :: IO ()
-main = defaultMain $ testGroup "All"
+main = defaultMain $ testGroup "All" $
   [ testGroup "Mod 1" $
     testProperty "fromInteger"
       (fromIntegerProp (Proxy :: Proxy 1)) :
     map lawsToTest (laws1 (Proxy :: Proxy (Mod 1)))
-  , testGroup "Mod 2310" $
-    testProperty "fromInteger"
-      (fromIntegerProp (Proxy :: Proxy 2310)) :
-    testProperty "invertMod"   (invertModProp   @2310) :
-    testProperty "powMod"      (powModProp      @2310) :
-    map lawsToTest (laws (Proxy :: Proxy (Mod 2310)))
-  , testGroup "Mod 18446744073709551615" $
-    testProperty "fromInteger"
-      (fromIntegerProp (Proxy :: Proxy 18446744073709551615)) :
-    testProperty "invertMod"   (invertModProp   @18446744073709551615) :
-    testProperty "powMod"      (powModProp      @18446744073709551615) :
-    map lawsToTest (laws (Proxy :: Proxy (Mod 18446744073709551615)))
-  , testGroup "Mod 18446744073709551626" $
-    testProperty "fromInteger"
-      (fromIntegerProp (Proxy :: Proxy 18446744073709551626)) :
-    testProperty "powMod"      (powModProp      @18446744073709551626) :
-    testProperty "invertMod"   (invertModProp   @18446744073709551626) :
-    map lawsToTest (laws (Proxy :: Proxy (Mod 18446744073709551626)))
-  , testGroup "Mod 123456789012345678901234567890" $
-    testProperty "fromInteger"
-      (fromIntegerProp (Proxy :: Proxy 123456789012345678901234567890)) :
-    testProperty "powMod"      (powModProp      @123456789012345678901234567890) :
-    testProperty "invertMod"   (invertModProp   @123456789012345678901234567890) :
-    map lawsToTest (laws (Proxy :: Proxy (Mod 123456789012345678901234567890)))
-  , testGroup "Mod 2 ^ 40000" $
-    testProperty "fromInteger"
-      (fromIntegerProp (Proxy :: Proxy (2 ^ 40000))) :
-    testProperty "powMod"      (powModProp      @(2 ^ 40000)) :
-    testProperty "invertMod"   (invertModProp   @(2 ^ 40000)) :
-    map lawsToTest (laws (Proxy :: Proxy (Mod (2 ^ 40000))))
+  , testMod(2310)
+  , testMod(2^16-1)
+  , testMod(2^16)
+  , testMod(2^16+1)
+  , testMod(2^32-1)
+  , testMod(2^32)
+  , testMod(2^32+1)
+  , testMod(2^64-1)
+  , testMod(2^64)
+  , testMod(2^64+1)
+  , testMod(123456789012345678901234567890)
+  , testModLabeled("2^40000", 2^40000)
   , testGroup "Random Mod"
     [ testProperty "fromInteger" fromIntegerRandomProp
     , testProperty "invertMod"   invertModRandomProp
@@ -102,27 +104,19 @@ main = defaultMain $ testGroup "All"
     testProperty "fromInteger"
       (fromIntegerWordProp (Proxy :: Proxy 1)) :
     map lawsToTest (laws1 (Proxy :: Proxy (Word.Mod 1)))
-  , testGroup "Word.Mod 2310" $
-    testProperty "fromInteger"
-      (fromIntegerWordProp (Proxy :: Proxy 2310)) :
-    testProperty "powMod"    (powModWordProp    @2310) :
-    testProperty "invertMod" (invertModWordProp @2310) :
-    map lawsToTest (laws (Proxy :: Proxy (Word.Mod 2310)))
-  , if finiteBitSize (0 :: Word) == 64 then
-      testGroup "Word.Mod 18446744073709551615" $
-      testProperty "fromInteger"
-        (fromIntegerWordProp (Proxy :: Proxy 18446744073709551615)) :
-      testProperty "powMod"    (powModWordProp    @18446744073709551615) :
-      testProperty "invertMod" (invertModWordProp @18446744073709551615) :
-      map lawsToTest (laws (Proxy :: Proxy (Word.Mod 18446744073709551615)))
-    else
-      testGroup "Word.Mod 4294967295" $
-      testProperty "fromInteger"
-        (fromIntegerWordProp (Proxy :: Proxy 4294967295)) :
-      testProperty "powMod"    (powModWordProp    @4294967295) :
-      testProperty "invertMod" (invertModWordProp @4294967295) :
-      map lawsToTest (laws (Proxy :: Proxy (Word.Mod 4294967295)))
-  , testGroup "Random Word.Mod"
+  , testMod(2310)
+  , testMod(2^16-1)
+  , testMod(2^16)
+  , testMod(2^16+1)
+  , testMod(2^32-1)
+  ] ++ if finiteBitSize (0 :: Word) /= 64 then [] else
+  [ testMod(2^32)
+  , testMod(2^32+1)
+  , testMod(2^64-1)
+  , testMod(2^64)
+  , testMod(2^64+1)
+  ] ++
+  [ testGroup "Random Word.Mod"
     [ testProperty "fromInteger" fromIntegerWordRandomProp
     , testProperty "invertMod"   invertModWordRandomProp
     , testProperty "invertMod near maxBound" invertModWordRandomPropNearMaxBound
